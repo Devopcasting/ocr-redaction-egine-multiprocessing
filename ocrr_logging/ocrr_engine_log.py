42abb409501d6613d7a5b2c370c44b54c7e739a3
Devopscasting <devopcasting@gmail.com>
2023-10-05 00:38:54 +0530

    added process id in log format

--- ocrr_logging/ocrr_engine_log.py
+++ ocrr_logging/ocrr_engine_log.py
@@ -2,21 +2,42 @@ import logging
 
 
 class OCRREngineLogging:
-    def __init__(self, log_file='C:\\Users\\pokhriyal\\Desktop\\Project-OCRR\\logs\ocrr_engine.log', log_level=logging.INFO) -> None:
-        self.log_file = log_file
+    def __init__(self, log_file_path=r'C:\Users\pokhriyal\Desktop\Project-OCRR\logs\ocrr_engine.log', log_level=logging.DEBUG):
+        self.log_file_path = log_file_path
         self.log_level = log_level
-    
-    def configure_logger(self):
-        logger = logging.getLogger(__name__)
-        logger.setLevel(self.log_level)
 
-        file_handler = logging.FileHandler(self.log_file)
-        file_handler.setLevel(self.log_level)
+        # Create a logger object
+        self.logger = logging.getLogger()
 
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # Set the log level
+        self.logger.setLevel(log_level)
+
+        # Create a file handler
+        file_handler = logging.FileHandler(log_file_path)
+
+        # Create a formatter
+        formatter = logging.Formatter('%(process)d %(asctime)s %(levelname)s %(message)s')
+
+        # Add the formatter to the file handler
         file_handler.setFormatter(formatter)
 
-        logger.addHandler(file_handler)
+        # Add the file handler to the logger
+        self.logger.addHandler(file_handler)
 
-        return logger
-    
+    def log(self, msg, level=logging.INFO):
+        self.logger.log(level, msg)
+
+    def debug(self, msg):
+        self.log(msg, logging.DEBUG)
+
+    def info(self, msg):
+        self.log(msg, logging.INFO)
+
+    def warning(self, msg):
+        self.log(msg, logging.WARNING)
+
+    def error(self, msg):
+        self.log(msg, logging.ERROR)
+
+    def critical(self, msg):
+        self.log(msg, logging.CRITICAL)
