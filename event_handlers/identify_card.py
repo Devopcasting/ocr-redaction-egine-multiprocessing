578566a8970a3b8f197640c59cc5b1ca5fd5f5a1
Devopscasting <devopcasting@gmail.com>
2023-10-06 12:39:39 +0530

    updated aadhaar card identification

--- event_handlers/identify_card.py
+++ event_handlers/identify_card.py
@@ -1,10 +1,12 @@
 import os
 import time
 import shutil
+import re
 import pytesseract
 from watchdog.events import FileSystemEventHandler
 from helpers.process_text import CleanText
 from helpers.identify_pan_card import IdentifyPanCard
+from helpers.identify_aadhaar_card import IdentifyAadhaarCard
 from ocrr_logging.ocrr_engine_log import OCRREngineLogging
 
 class IdentifyCard(FileSystemEventHandler):
@@ -20,11 +22,20 @@ class IdentifyCard(FileSystemEventHandler):
         # Get file name
         image_file_name = os.path.basename(image_path)
 
+        # Rejected image path
+        rejected_image_path = r'C:\Users\pokhriyal\Desktop\Project-OCRR\images\rejected_images'
+
         # Pan card Pattern1 path
         pan_card_p1_path = r'C:\Users\pokhriyal\Desktop\Project-OCRR\images\pan_card\pattern1'
 
         # Pan card Pattern2 path
         pan_card_p2_path = r'C:\Users\pokhriyal\Desktop\Project-OCRR\images\pan_card\pattern2'
+
+        # Aadhaar card front path
+        aadhaar_card_front_path = r'C:\Users\pokhriyal\Desktop\Project-OCRR\images\aadhaar_card\front'
+
+        # Aadhaar card  back path
+        aadhaar_card_back_path = r'C:\Users\pokhriyal\Desktop\Project-OCRR\images\aadhaar_card\back'
 
         # Wait for 1 second before reading the file
         while True:
@@ -41,8 +52,10 @@ class IdentifyCard(FileSystemEventHandler):
         data_text = pytesseract.image_to_string(image_path, output_type=pytesseract.Output.DICT,config=tesseract_config)
         clean_text = CleanText(data_text).clean_text()
 
-        # Check for Pan card
+        # Check for Pan card or Aadhaar card
         pan_card = IdentifyPanCard(clean_text)
+        aadhaar_card = IdentifyAadhaarCard(clean_text)
+
         if pan_card.check_pan_card():
             if pan_card.identify_pan_card_pattern_1():
                 self.logger.info(f"Pan card {image_file_name} is of pattern 1")
@@ -50,5 +63,15 @@ class IdentifyCard(FileSystemEventHandler):
             else:
                 self.logger.info(f"Pan card {image_file_name} is of pattern 2")
                 shutil.move(image_path, os.path.join(pan_card_p2_path, image_file_name))
+        elif aadhaar_card.check_aadhaar_card():
+            if aadhaar_card.check_aadhaar_front():
+                self.logger.info(f"Aadhaar card {image_file_name} front side")
+                shutil.move(image_path, os.path.join(aadhaar_card_front_path, image_file_name))
+            else:
+                self.logger.info(f"Aadhaar card {image_file_name} back side")
+                shutil.move(image_path, os.path.join(aadhaar_card_back_path, image_path))
+        else:
+            self.logger.error("ERR002")
+            shutil.move(image_path, os.path.join(rejected_image_path, image_file_name))
 
     
